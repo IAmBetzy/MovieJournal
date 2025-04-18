077823bb1b45a0364aebd3732e7879acc88f8cf5
Carolina Fernandez <carolinafm9d@gmail.com>
2025-04-17 22:04:29 -0700

    a

---
+++ Review+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Review+CoreDataClass.swift
+//  MovieJournal
+//
+//  Created by Claudia Moca on 17/04/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Review)
+public class Review: NSManagedObject {
+
+}
